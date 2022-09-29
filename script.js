cc1cd73a7fb12342841ee4fb1819bfa75dcbd16f
Hassan <tayehassan0000@gmail.com>
2022-09-29 02:21:05 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const backgroundImages=['img/restaurant 1.png','img/Rectangle 14 breakkie roll.png','img/pacakes.png']
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const backgroundImages = [
+  "img/restaurant1.png",
+  "img/breakkie-roll.png",
+  "img/pacakes.png",
+];
+
+prev.classList.add("hidden");
+next.classList.add("hidden");
+
+const changeBackgroundImage = function (curBackground) {
+    
+  if (curBackground === 0) {
+    next.classList.remove("hidden");
+  } else if (curBackground === 2) {
+    prev.classList.remove("hidden");
+  } else {
+    next.classList.remove("hidden");
+    prev.classList.remove("hidden");
+  }
+
+  const curBackgroundImage = (document.querySelector(
+    ".home"
+  ).style.background = `top left / cover url(${backgroundImages[curBackground]} )no-repeat`);
+};
+
+changeBackgroundImage(2);
