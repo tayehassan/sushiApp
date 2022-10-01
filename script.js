b5cd1a5dee8a9e4c3a88e778077dd4cf71d3305a
Hassan <tayehassan0000@gmail.com>
2022-10-01 17:32:49 +0100

    update

--- script.js
+++ script.js
@@ -6,23 +6,41 @@ const backgroundImages = [
   "img/pacakes.png",
 ];
 
+let curBackground = 0;
+
 prev.classList.add("hidden");
 next.classList.add("hidden");
 
-const changeBackgroundImage = function (curBackground) {
-    
-  if (curBackground === 0) {
+const changeBackgroundImage = function () {
+  if (curBackground == 0) {
     next.classList.remove("hidden");
-  } else if (curBackground === 2) {
+    prev.classList.add("hidden");
+  } else if (curBackground == 2) {
     prev.classList.remove("hidden");
+    next.classList.add("hidden");
   } else {
     next.classList.remove("hidden");
     prev.classList.remove("hidden");
   }
+
+  // console.log(typeof curBackground);
 
   const curBackgroundImage = (document.querySelector(
     ".home"
   ).style.background = `top left / cover url(${backgroundImages[curBackground]} )no-repeat`);
 };
 
-changeBackgroundImage(2);
+window.addEventListener('load',
+changeBackgroundImage)
+
+prev.addEventListener("click", function () {
+  curBackground = curBackground - 1;
+  changeBackgroundImage();
+});
+next.addEventListener("click", function () {
+  curBackground = curBackground + 1;
+  changeBackgroundImage();
+});
+
+const menu=function(){document.querySelector('.fa-utensils').style.background='rgb(225,160,122)'
+}
